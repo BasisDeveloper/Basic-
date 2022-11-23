d4e6a319996680e141f8af640d9b72401885650d
Colin M <108040634+BasisDeveloper@users.noreply.github.com>
2022-11-23 05:24:54 -0500

    Fixed a logical error in EXPECT macro
    Conditions passed into the EXPECT macro were being evaluated twice.

--- include/Basic++/Expectations.hxx
+++ include/Basic++/Expectations.hxx
@@ -1,6 +1,5 @@
 #pragma once
 
-
 #include <source_location>
 #include <filesystem>
 
@@ -17,28 +16,33 @@
 
 namespace Basic
 {
-#if defined(BASIC_PCH)
-  static
-#endif
-    void Expect(
-      bool condition,
-      const Message&& msg,
-      const std::source_location& source_location = std::source_location::current())
-  {
-    if (!condition) [[unlikely]]
-    {
-      Basic::Logging::Print("expectation not satisfied : {}", msg.string.data());
+	#if defined(BASIC_PCH)
+	static
+	#endif
+		bool Expect(
+			bool condition,
+			const Message&& msg,
+			const std::source_location& source_location = std::source_location::current())
+	{
+		if (!condition) [[unlikely]]
+		{
+			Basic::Logging::Print("expectation not satisfied : {}", msg.string.data());
 
-      const auto file_name =
-        std::filesystem::path(source_location.file_name()).filename().string();
+			const auto file_name =
+				std::filesystem::path(source_location.file_name()).filename().string();
 
-      const char* function_name = source_location.function_name();
+			const char* function_name = source_location.function_name();
 
-      Basic::Logging::Println(" ~ file:'{}', function:'{}', line:'{}:{}'",
-        file_name.data(), function_name, source_location.line(), source_location.column());
-    }
-  }
+			Basic::Logging::Println(" ~ file:'{}', function:'{}', line:'{}:{}'",
+				file_name.data(), function_name, source_location.line(), source_location.column());
+		}
+
+		return condition;
+	}
 }
 
-#define EXPECT(_cond_, _msg_, ...) Basic::Expect(_cond_, {_msg_, __VA_ARGS__}, std::source_location::current()); \
-        if (!(_cond_)) {BASIC_DEBUG_BREAK(); std::exit(EXIT_FAILURE);};
+#define EXPECT(_cond_, _msg_, ...) \
+        if (!Basic::Expect((_cond_), {_msg_, __VA_ARGS__}, std::source_location::current())) \
+		{																					 \
+			BASIC_DEBUG_BREAK(); std::exit(EXIT_FAILURE);									 \
+		}; 																					 \
